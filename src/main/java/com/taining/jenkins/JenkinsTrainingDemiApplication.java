a4cb69a1fba0754735d51ccc6eac816184cf07c0
rahulnimbalkar23 <rahulnimbalkar247@gmail.com>
2023-06-07 09:19:24 +0000

    first comiit

---
+++ src/main/java/com/taining/jenkins/JenkinsTrainingDemiApplication.java
@@ -0,0 +1,13 @@
+package com.taining.jenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsTrainingDemiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsTrainingDemiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/taining/jenkins/JenkinsTrainingDemiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taining.jenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsTrainingDemiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
